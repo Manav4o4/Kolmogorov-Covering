cf7e02872583fd9805a2a4c7a196d23ae095c46c
Manav Bajaj <manavbajaj253@gmail.com>
2024-11-19 14:43:02 -0500

    Final Fix

--- kolmogorov_covering.py
+++ kolmogorov_covering.py
@@ -2,90 +2,87 @@ import math, torch
 import numpy as np
 from itertools import combinations
 
-class Kolmogorov_graph_covering:
+def kolmogorov_covering(graph, model):
 
-    def __init__(self, graph):
+    vertices = len(graph)
 
-        self.graph = graph
-        self.vertices = len(graph)
+    complexity = math.comb(vertices, 2)
 
-    def kolmogorov_covering(self, graph, model):
+    subgraphs_iterated_over = 0
 
-        vertices = len(graph)
+    for i in range(vertices, math.ceil(2*math.log(vertices, 2) + 5), -1):
 
-        complexity = math.comb(vertices, 2)
+        if len(graph) < math.ceil(2*math.log(vertices, 2) + 5): # If |graph| < |subgraph|, then break
 
-        subgraphs_iterated_over = 0
+            break
 
-        for i in range(vertices, math.ceil(2*math.log(vertices, 2) + 5), -1):
+        graph_dict = matrix_to_dict(graph)
 
-            graph_dict = self.matrix_to_dict(graph)
+        subsets = list(combinations(list(graph_dict.keys()), i)) # Gives us all possible subseuquences of the lebelling of size 'i'
 
-            subsets = list(combinations(list(graph_dict.keys()), i)) # Gives us all possible subseuquences of the lebelling of size 'i'
+        print(len(subsets))
 
-            print(len(subsets))
+        subgraphs_iterated_over += len(subsets)
 
-            subgraphs_iterated_over += len(subsets)
+        for subgraph in subsets:
 
-            for subgraph in subsets:
+            subgraph_bitstring = create_bitstring(subgraph, graph_dict) # Creates the bitstring for the current subgraph
 
-                subgraph_bitstring = self.create_bitstring(subgraph, graph_dict) # Creates the bitstring for the current subgraph
+            with torch.no_grad():
+                output = model(string_to_tensor(subgraph_bitstring))
 
-                with torch.no_grad():
-                    output = model(self.string_to_tensor(subgraph_bitstring))
+            # print(output.item(), i)
 
-                # print(output.item(), i)
+            if output.item() > 0.5: # If the graph is basic, then update the complexity, graph_dict
 
-                if output.item() > 0.5: # If the graph is basic, then update the complexity, graph_dict
+                for vertex in range(len(subgraph)-1, 0, -1):
 
-                    for vertex in range(len(subgraph)-1, 0, -1):
+                    numpy_graph = np.array(graph)
 
-                        numpy_graph = np.array(graph)
+                    graph = delete_vertex(numpy_graph, subgraph[vertex-1]).tolist()
 
-                        graph = self.delete_vertex(numpy_graph, subgraph[vertex-1]).tolist()
+                graph_dict = matrix_to_dict(graph)
 
-                    graph_dict = self.matrix_to_dict(graph)
+                complexity -= math.comb(len(subgraph), 2)
 
-                    complexity -= math.comb(len(subgraph), 2)
+                if len(graph) < math.ceil(2*math.log(vertices, 2) + 5): # If |graph| < |subgraph|, then break
 
-                    if len(graph) < math.ceil(2*math.log(vertices, 2) + 5): # If |graph| < |subgraph|, then break
+                    break
 
-                        break
+    return complexity, subgraphs_iterated_over
 
-        return complexity, subgraphs_iterated_over
+def matrix_to_dict(graph):
+
+    result_dict = {}
+
+    for i in range(len(graph)):
+        # Join each row (list) into a string and assign it to the dictionary
+        result_dict[i + 1] = "".join(map(str, graph[i]))
+
+    return result_dict
+
+def string_to_tensor(str):
+
+    sequence = torch.tensor([int(bit) for bit in str], dtype=torch.float32)
+
+    return sequence.view(1, -1, 1)
+
+def delete_vertex(graph, vertex):
+
+    graph = np.delete(graph, vertex, axis=0)  # Delete the row
+    graph = np.delete(graph, vertex, axis=1)  # Delete the column
+    return graph
+
+def create_bitstring(subgraph, graph_dict):
+
+    subgraph_bitstring = ''
+
+    for vertex in subgraph:
+
+        subgraph_bitstring = subgraph_bitstring + graph_dict[vertex][vertex:] # Builds the substring of the current subgraph
+
+    return subgraph_bitstring
+            
+
+
     
-    def matrix_to_dict(self):
-
-        result_dict = {}
-
-        for i in range(len(self.graph)):
-            # Join each row (list) into a string and assign it to the dictionary
-            result_dict[i + 1] = "".join(map(str, self.graph[i]))
-
-        return result_dict
-
-    def string_to_tensor(self, str):
-
-        sequence = torch.tensor([int(bit) for bit in str], dtype=torch.float32)
-
-        return sequence.view(1, -1, 1)
-
-    def delete_vertex(self, vertex):
-
-        self.graph = np.delete(self.graph, vertex, axis=0)  # Delete the row
-        self.graph = np.delete(self.graph, vertex, axis=1)  # Delete the column
-        return self.graph
-
-    def create_bitstring(self, subgraph, graph_dict):
-
-        subgraph_bitstring = ''
-
-        for vertex in subgraph:
-
-            subgraph_bitstring = subgraph_bitstring + graph_dict[vertex][vertex:] # Builds the substring of the current subgraph
-
-        return subgraph_bitstring
-                
-
-
-        
--- testing_outputs.py
+++ testing_outputs.py
@@ -3,18 +3,19 @@ from Generate_data_set.generate_random_graphs import generate_erdos_renyi_graph
 import math
 import matplotlib.pyplot as plt
 from RNN import model
+import os
 
 model.eval()
 
 def k_covering_fifty():
-    vertices = 20
 
+    vertices = 30
     k_complexity_list = []
     prob_list = []
     iteration_list = []
     original_complexity_list = []
 
-    for i in range(1, 6, 1):
+    for i in range(1, 11, 1):
 
         prob = i/10
 
@@ -45,14 +46,26 @@ def k_covering_fifty():
     
     plt.figure(figsize=(8, 6))
     plt.plot(prob_list, k_complexity_list, label='K-Complexity', marker='o', color='blue')
-    #plt.plot(prob_list, iteration_list, label='Number of Iterations', marker='x', color='orange')
     plt.plot(prob_list, original_complexity_list, label='Starting Complexity', marker='p', color='red')
-    
     plt.xlabel('Probability')
     plt.ylabel('Values')
-    plt.title('K-Complexity and Number of Iterations vs Probability')
-    plt.legend()  # Show the legend
-    plt.grid(True)  # Add grid for better readability
+    plt.title('K-Complexity and Starting Complexity vs Probability')
+    plt.legend()
+    save_path = os.path.join('Outputs', "k_complexity_vs_probability_30.png")
+    plt.savefig(save_path)
+    plt.grid(True)
+    plt.show()
+
+    # New figure for iterations
+    plt.figure(figsize=(8, 6))
+    plt.plot(prob_list, iteration_list, label='Number of Iterations', marker='x', color='orange')
+    plt.xlabel('Probability')
+    plt.ylabel('Number of Iterations')
+    plt.title('Number of Iterations vs Probability')
+    plt.legend()
+    save_path = os.path.join('Outputs', "iteratations_vs_probability_30.png")
+    plt.savefig(save_path)
+    plt.grid(True)
     plt.show()
 
 k_covering_fifty()
